b529daf513a82fc21927d834a7013ffb105e5499
Efe Yücel Bozkurt <ebozkurt056@outlook.com>
2024-07-23 19:42:46 +0300

    Update genelbakis_screen.dart

--- lib/screens/genelbakis_screen.dart
+++ lib/screens/genelbakis_screen.dart
@@ -42,30 +42,6 @@ class _GenelBakisScreenState extends State<GenelBakisScreen> {
     super.dispose();
   }
 
-  Future<void> _fetchCoinData() async {
-    if (_isDataLoaded) return;
-
-    try {
-      final data = await Provider.of<PortfolioProvider>(context, listen: false)
-          .fetchCoinData();
-      setState(() {
-        _coinData = data;
-        _isDataLoaded = true;
-        _hasError = false; // Hata durumu sıfırlanır
-      });
-    } catch (e) {
-      setState(() {
-        _hasError = true;
-      });
-
-      if (e.toString().contains('429')) {
-        _retryTimer = Timer(const Duration(seconds: 30), () {
-          _fetchCoinData();
-        });
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,130 +59,34 @@ class _GenelBakisScreenState extends State<GenelBakisScreen> {
                         child: AdWidget(ad: _googleAds.bannerAd!),
                       )),
 
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 20, 0, 0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Genel Bakış",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: HexColor(black1),
-                    fontSize: 22,
-                  ),
-                ),
-              ),
-            ),
+            genelBakisText(),
 
             //Portfolio Value
             Expanded(
               flex: 4,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Consumer<PortfolioProvider>(
-                    builder: (context, portfolioProvider, child) {
-                      final transactions = portfolioProvider.allTransactions;
-                      final totalValue = portfolioProvider.totalValue;
-                      return transactions.isNotEmpty && totalValue == 0
-                          ? const CircularProgress()
-                          : Text(
-                              '₺${totalValue.toStringAsFixed(0)}',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: HexColor(black1),
-                                fontSize: 35,
-                              ),
-                            );
-                    },
-                  ),
-                ),
-              ),
+              child: showPortfolioValue(),
             ),
             //Selectionbox
             Expanded(
               flex: 3,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: SelectionBox(onSelectionChanged: _onSelectionChanged),
-              ),
+              child: selectionBox(),
             ),
             //Grafik yazi
             Expanded(
               flex: 2,
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                      padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
-                      child: Text(
-                        'Bitcoin Grafiği (\$)',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 15,
-                            color: HexColor(black1)),
-                      ))),
+              child: btcGraphText(),
             ),
             //Bitcoin Chart
             Expanded(
               flex: 15,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: SizedBox(
-                  child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 35),
-                      child: PortfolioChart(selectedTime: _selectedTime)),
-                ),
-              ),
+              child: btcChart(),
             ),
             //Baslik Text
-            const Expanded(
-                flex: 6,
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(15, 40, 0, 0),
-                      child: Text(
-                        'Son 24 Saatte Fiyat Hareketleri',
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.w800),
-                      ),
-                    ))),
+            Expanded(flex: 6, child: lastDayPerformanceTitle()),
             //Kutucuk Üstü Text
-            const Expanded(
+            Expanded(
               flex: 2,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        'Bitcoin',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        'En çok yükselen',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        'En çok düşen',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: aboveBoxTitle(),
             ),
             //Bilgi kutucukları
             Expanded(
@@ -215,26 +95,143 @@ class _GenelBakisScreenState extends State<GenelBakisScreen> {
                   ? const Center(child: CircularProgress())
                   : _coinData == null
                       ? const Center(child: CircularProgress())
-                      : Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 0),
-                          child: Align(
-                            alignment: Alignment.topCenter,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                _buildCoinInfo(
-                                    _coinData!['bitcoin'], 'Bitcoin'),
-                                _buildCoinInfo(
-                                    _coinData!['topGainer'], 'En Çok Yükselen'),
-                                _buildCoinInfo(
-                                    _coinData!['topLoser'], 'En Çok Düşen'),
-                              ],
-                            ),
-                          ),
-                        ),
+                      : infoBoxes(),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Padding infoBoxes() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildCoinInfo(_coinData!['bitcoin'], 'Bitcoin'),
+            _buildCoinInfo(_coinData!['topGainer'], 'En Çok Yükselen'),
+            _buildCoinInfo(_coinData!['topLoser'], 'En Çok Düşen'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row aboveBoxTitle() {
+    return const Row(
+      children: [
+        Expanded(
+          child: Center(
+            child: Text(
+              'Bitcoin',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Text(
+              'En çok yükselen',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Text(
+              'En çok düşen',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Align lastDayPerformanceTitle() {
+    return const Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15, 40, 0, 0),
+          child: Text(
+            'Son 24 Saatte Fiyat Hareketleri',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
+          ),
+        ));
+  }
+
+  Align btcChart() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SizedBox(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 35),
+            child: PortfolioChart(selectedTime: _selectedTime)),
+      ),
+    );
+  }
+
+  Align btcGraphText() {
+    return Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+            child: Text(
+              'Bitcoin Grafiği (\$)',
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                  color: HexColor(black1)),
+            )));
+  }
+
+  Align selectionBox() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SelectionBox(onSelectionChanged: _onSelectionChanged),
+    );
+  }
+
+  Padding showPortfolioValue() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Consumer<PortfolioProvider>(
+          builder: (context, portfolioProvider, child) {
+            final transactions = portfolioProvider.allTransactions;
+            final totalValue = portfolioProvider.totalValue;
+            return transactions.isNotEmpty && totalValue == 0
+                ? const CircularProgress()
+                : Text(
+                    '₺${totalValue.toStringAsFixed(0)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: HexColor(black1),
+                      fontSize: 35,
+                    ),
+                  );
+          },
+        ),
+      ),
+    );
+  }
+
+  Padding genelBakisText() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 20, 0, 0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          "Genel Bakış",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: HexColor(black1),
+            fontSize: 22,
+          ),
         ),
       ),
     );
@@ -325,5 +322,29 @@ class _GenelBakisScreenState extends State<GenelBakisScreen> {
         ),
       ],
     );
+  }
+
+  Future<void> _fetchCoinData() async {
+    if (_isDataLoaded) return;
+
+    try {
+      final data = await Provider.of<PortfolioProvider>(context, listen: false)
+          .fetchCoinData();
+      setState(() {
+        _coinData = data;
+        _isDataLoaded = true;
+        _hasError = false;
+      });
+    } catch (e) {
+      setState(() {
+        _hasError = true;
+      });
+
+      if (e.toString().contains('429')) {
+        _retryTimer = Timer(const Duration(seconds: 30), () {
+          _fetchCoinData();
+        });
+      }
+    }
   }
 }
